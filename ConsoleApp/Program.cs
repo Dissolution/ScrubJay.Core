13ecc12ea2969c5057a35caecab93b8e21c76334
Timothy Jay <TimothyRJay@gmail.com>
2024-11-24 11:02:40 -0800

    0.9.2-alpha

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,5 +2,31 @@
 // ReSharper disable UnusedVariable
 
 using System.Diagnostics;
+using ScrubJay.Extensions;
+
+
+// var str = string.Join(", ", Test.GetEnumerator(..4));
+
+foreach (var i in 2..)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
 
 Debugger.Break();
+return;
+
+
+
+
+
+class StructWrapper<T>
+    where T : struct;
+
+
+static class Test
+{
+
+}
---
+++ src/Utilities/TypeNames.cs
@@ -0,0 +1,170 @@
+#pragma warning disable IDE0058
+
+using ScrubJay.Collections;
+using InterpolatedText = System.Runtime.CompilerServices.DefaultInterpolatedStringHandler;
+
+namespace ScrubJay.Utilities;
+
+/// <summary>
+/// A helper for rendering <see cref="Type"/> names
+/// </summary>
+[PublicAPI]
+public static class TypeNames
+{
+    private static readonly ConcurrentTypeMap<string> _typeNameCache;
+
+    static TypeNames()
+    {
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/
+        _typeNameCache = new()
+        {
+            // C# type aliases
+            [typeof(bool)] = "bool",
+            [typeof(char)] = "char",
+            [typeof(sbyte)] = "sbyte",
+            [typeof(byte)] = "byte",
+            [typeof(short)] = "short",
+            [typeof(ushort)] = "ushort",
+            [typeof(int)] = "int",
+            [typeof(uint)] = "uint",
+            [typeof(long)] = "long",
+            [typeof(ulong)] = "ulong",
+            [typeof(float)] = "float",
+            [typeof(double)] = "double",
+            [typeof(decimal)] = "decimal",
+            [typeof(string)] = "string",
+            [typeof(object)] = "object",
+            [typeof(void)] = "void",
+            [typeof(nint)] = "nint",
+            [typeof(nuint)] = "nuint",
+        };
+    }
+
+    private static void AppendTypeName(
+        this ref InterpolatedText name,
+        Type? type)
+    {
+        if (type is null)
+        {
+            name.AppendLiteral("null");
+            return;
+        }
+
+        // Enum types are their NameFrom
+        if (type.IsEnum)
+        {
+            name.AppendLiteral(type.Name);
+            return;
+        }
+
+        // Nullable<T> => T?
+        Type? underType = Nullable.GetUnderlyingType(type);
+        if (underType is not null)
+        {
+            // c# Nullable alias
+            name.AppendTypeName(underType);
+            name.AppendLiteral("?");
+            return;
+        }
+
+        // Pointer -> utype*
+        if (type.IsPointer)
+        {
+            underType = type.GetElementType()!;
+            name.AppendTypeName(underType);
+            name.AppendLiteral("*");
+            return;
+        }
+
+        // ByRef => ref type
+        if (type.IsByRef)
+        {
+            underType = type.GetElementType()!;
+            name.AppendLiteral("ref ");
+            name.AppendTypeName(underType);
+            return;
+        }
+
+        // Array => T[]
+        if (type.IsArray)
+        {
+            underType = type.GetElementType()!;
+            name.AppendTypeName(underType);
+            name.AppendLiteral("[]");
+            return;
+        }
+
+        // Nested Type?
+        if (type.IsNested && !type.IsGenericParameter)
+        {
+            name.AppendTypeName(type.DeclaringType);
+            name.AppendLiteral(".");
+        }
+
+        // If non-generic
+        if (!type.IsGenericType)
+        {
+            // Just write the type name and we're done
+            name.AppendLiteral(type.Name);
+        }
+        else
+        {
+            // Start processing type name
+            ReadOnlySpan<char> typeName = type.Name.AsSpan();
+
+            /* The default NameFrom for a generic type is:
+             * Thing<>   = Thing`1
+             * Thing<,>  = Thing`2
+             * Thing<,,> = Thing`3
+             * ...
+             */
+            var i = typeName.IndexOf('`');
+            if (i >= 0)
+            {
+                name.AppendFormatted(typeName[..i]);
+            }
+            else
+            {
+                // Odd... use the name
+                name.AppendFormatted(typeName);
+            }
+
+            // Add our generic types to finish
+            var argTypes = type.GetGenericArguments();
+            int argCount = argTypes.Length;
+            Debug.Assert(argCount > 0);
+
+            name.AppendLiteral("<");
+            name.AppendTypeName(argTypes[0]);
+            for (i = 1; i < argCount; i++)
+            {
+                name.AppendLiteral(", ");
+                name.AppendTypeName(argTypes[i]);
+            }
+
+            name.AppendLiteral(">");
+        }
+    }
+
+    private static string CreateTypeName(Type? type)
+    {
+        var text = new InterpolatedText(32, 1);
+        AppendTypeName(ref text, type);
+        return text.ToStringAndClear();
+    }
+
+    /// <summary>
+    /// Gets the rendered name of this <see cref="Type"/>
+    /// </summary>
+    public static string NameOf(this Type? type)
+    {
+        if (type is null)
+            return "null";
+        return _typeNameCache.GetOrAdd(type, static t => CreateTypeName(t));
+    }
+
+    /// <summary>
+    /// Gets the rendered name of a generic type
+    /// </summary>
+    public static string NameOf<T>() => _typeNameCache.GetOrAdd<T>(static t => CreateTypeName(t));
+}
