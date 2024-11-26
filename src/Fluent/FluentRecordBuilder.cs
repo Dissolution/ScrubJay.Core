e080a33d00015bbd4b86394281e51792d064b025
Timothy Jay <TimothyRJay@gmail.com>
2024-11-26 14:09:45 -0800

    Clean

--- src/Fluent/FluentRecordBuilder.cs
+++ src/Fluent/FluentRecordBuilder.cs
@@ -1,10 +1,11 @@
-﻿#pragma warning disable CA1715
+﻿using ScrubJay.Comparison;
+
+#pragma warning disable CA1715
 
 namespace ScrubJay.Fluent;
 
 [PublicAPI]
-public abstract class FluentRecordBuilder<B, R> : FluentBuilder<B>,
-    IEquatable<R>
+public abstract class FluentRecordBuilder<B, R> : FluentBuilder<B>
     where B : FluentRecordBuilder<B, R>
     where R : class
 {
@@ -27,24 +28,9 @@ public abstract class FluentRecordBuilder<B, R> : FluentBuilder<B>,
         _record = record;
     }
 
-    public virtual B Execute(Action<B, R> buildWithRecord)
+    public virtual B Execute(Action<B, R>? buildWithRecord)
     {
-        buildWithRecord.Invoke(_builder, _record);
+        buildWithRecord?.Invoke(_builder, _record);
         return _builder;
     }
-
-    public bool Equals(R? record) => EqualityComparer<R>.Default.Equals(_record!, record!);
-
-    public override bool Equals([NotNullWhen(true)] object? obj)
-    {
-        if (obj is B)
-            return ReferenceEquals(this, obj);
-        if (obj is R record)
-            return Equals(record);
-        return false;
-    }
-
-    public override int GetHashCode() => Hasher.GetHashCode<R>(_record);
-
-    public override string ToString() => $"{typeof(B).Name}<{typeof(R).Name}>";
 }
